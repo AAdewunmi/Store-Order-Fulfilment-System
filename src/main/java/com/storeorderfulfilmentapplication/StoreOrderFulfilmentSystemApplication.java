a014df71b5b37201f49ebf1c3d76881aff3768f1
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-11 11:56:02 +0000

    Initial Project Commit!

---
+++ src/main/java/com/storeorderfulfilmentapplication/StoreOrderFulfilmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.storeorderfulfilmentapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOrderFulfilmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreOrderFulfilmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storeorderfulfilmentapplication/StoreOrderFulfilmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storeorderfulfilmentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOrderFulfilmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
