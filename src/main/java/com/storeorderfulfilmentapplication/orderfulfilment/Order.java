03ad1722bb0003dc208b5f94aa4f7169cf3924f3
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-11 15:31:49 +0000

    Fourth Commit: Implement Variable Declaration for Order Class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/Order.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/Order.java
@@ -1,5 +1,18 @@
 package com.storeorderfulfilmentapplication.orderfulfilment;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
 public class Order {
+	
+	private @Id @GeneratedValue Long id;
+	private String description;
+	private Status status;
+	
+	
 
 }
