20799b0f60557bbfe59ca430ea3cc273eab1c8d2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-12 14:09:12 +0000

    Seventh Commit: Implement getters and setters for Order Class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/Order.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/Order.java
@@ -19,6 +19,30 @@ public class Order {
 		this.description = description;
 		this.status = status;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 	
 	
 
