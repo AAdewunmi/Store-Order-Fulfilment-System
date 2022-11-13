c61424f2e4abc426308947c7d940ee1fa6dd8948
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-13 09:48:40 +0000

    Fifteenth Commit: Create variables for OrderController class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
@@ -1,5 +1,11 @@
 package com.storeorderfulfilmentapplication.orderfulfilment;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+	
+	private final OrderRepository orderRepository;
+	private final OrderModelAssembler assembler;
 
 }
