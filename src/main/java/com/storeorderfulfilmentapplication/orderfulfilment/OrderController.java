d8b05dd016558f8636a5b3715d80bd0b03ce102a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-14 09:17:25 +0000

    Nineteenth Commit: Implement constructor with fields for OrderController class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
@@ -7,5 +7,12 @@ public class OrderController {
 	
 	private final OrderRepository orderRepository;
 	private final OrderModelAssembler assembler;
+	
+	public OrderController(OrderRepository orderRepository, OrderModelAssembler assembler) {
+		this.orderRepository = orderRepository;
+		this.assembler = assembler;
+	}
+	
+	
 
 }
