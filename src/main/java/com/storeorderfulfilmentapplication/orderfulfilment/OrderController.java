85b6f9f67e0dffb498ad39b351841a191b379dc4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-14 17:05:34 +0000

    Twenty-Four Commit: Implement newOrder() method in OrderController class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderController.java
@@ -5,8 +5,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
@@ -33,10 +36,20 @@ public class OrderController {
 	EntityModel<Order> one(@PathVariable Long id){
 		Order order = orderRepository.findById(id)
 				.orElseThrow(() -> new OrderNotFoundException(id));
-				
+		
+		return assembler.toModel(order);		
 	}
 	
-	
+	@PostMapping("/orders")
+	ResponseEntity<EntityModel<Order>> newOrder(@RequestBody Order order){
+		order.setStatus(Status.IN_PROGRESS);
+		Order newOrder = orderRepository.save(order);
+		
+		return ResponseEntity
+				
+		.created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
+		.body(assembler.toModel(newOrder));
+	}
 	
 	
 }
