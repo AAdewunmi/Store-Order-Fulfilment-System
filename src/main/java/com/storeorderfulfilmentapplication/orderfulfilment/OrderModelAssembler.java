256e1b5e1f19bead71b3ff4ac76ba1e360712503
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-14 08:22:39 +0000

    Eighteen Commit: Implement toModel() in OrderModelAssembler Class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderModelAssembler.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/OrderModelAssembler.java
@@ -1,5 +1,26 @@
 package com.storeorderfulfilmentapplication.orderfulfilment;
 
-public class OrderModelAssembler {
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>>{
+
+	@Override
+	public EntityModel<Order> toModel(Order order) {
+		EntityModel<Order> orderModel = EntityModel.of(order,
+				linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+				linkTo(methodOn(OrderController.class).all()).withRel("Orders"));
+		
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			orderModel.add(linkTo(methodOn(OrderController.class).cancel(order.getId())).withRel("Cancel"));
+			orderModel.add(linkTo(methodOn(OrderController.class).complete(order.getId())).withRel("Complete"));
+		}
+		return orderModel;
+	}
 
 }
