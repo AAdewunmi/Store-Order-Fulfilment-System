e194d21de83edf2048a8141ffc5dd9490512868a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-11-15 10:21:34 +0000

    Twenty-Eight Commit: Implement LoadDatabase Class

--- src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/LoadDatabase.java
+++ src/main/java/com/storeorderfulfilmentapplication/orderfulfilment/LoadDatabase.java
@@ -1,5 +1,26 @@
 package com.storeorderfulfilmentapplication.orderfulfilment;
 
-public class LoadDatabase {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class LoadDatabase {
+	
+	private static final Logger log= LoggerFactory.getLogger(LoadDatabase.class);
+	
+	@Bean
+	CommandLineRunner initDatabase(OrderRepository orderRepository) {
+		
+		return args -> {
+			orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+			orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+			
+			orderRepository.findAll().forEach(order -> {log.info("Preloaded " + order);
+			});
+		};
+		
+	}
 }
